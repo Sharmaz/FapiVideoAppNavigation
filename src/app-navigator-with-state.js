6b483517da8562dca51ba1f63e6276a1be033248
Ivan Robles <irae45@gmail.com>
2018-10-22 15:18:26 -0500

    Implementación del botón de back de los dispositivos con Android

--- src/app-navigator-with-state.js
+++ src/app-navigator-with-state.js
@@ -2,8 +2,9 @@ import { connect } from 'react-redux';
 import {
   reduxifyNavigator
 } from 'react-navigation-redux-helpers';
-
+import { BackHandler } from 'react-native';
 import AppNavigator from './app-navigator';
+import { NavigationActions } from 'react-navigation';
 
 /**
  * Utilizamos reduxifyNavigator el cual nos retorna un HOC
@@ -13,7 +14,26 @@ const ReduxifyApp = reduxifyNavigator(AppNavigator, 'root');
 
 // Extendemos de el HOC ReduxyfyApp, el cual va a contener el dispatch y el state
 class AppNavigatorWithState extends ReduxifyApp {
-
+  componentDidMount() {
+    /**
+     * Utilizamos de react-native Backhandler escuchando el evento hardwareBackPress
+     * le pasamos la función onBackPress para que se ejecute
+     */ 
+    BackHandler.addEventListener('hardwareBackPress', this.onBackPress);
+  }
+  componentWillUnmount() {
+    // Cuando el componente se desmonte dejamos de escuchar ese evento
+    BackHandler.removeEventListener('hardwareBackPress', this.onBackPress);
+  }
+  onBackPress = () => {
+    // Despachamos con redux via props el NavigationActions de back
+    this.props.dispatch(
+      NavigationActions.back({
+        key: null
+      })
+    );
+    return true;
+  }
 }
 
 // retornamos como state el state.navigation
